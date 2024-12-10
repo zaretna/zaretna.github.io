ca9488a4b118c2fd098ca1f9d0e09bfdf94f49a1
zaretna <angelazaretna@gmail.com>
2024-12-10 09:31:34 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-/*========== menu icon navbar ==========*/
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -7,8 +6,6 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-
-/*========== scroll sections active link ==========*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -27,38 +24,30 @@ window.onscroll = () => {
         };
     });
 
-
-/*========== sticky navbar ==========*/
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
 
-
-/*========== remove menu icon navbar when click navbar link (scroll) ==========*/
 menuIcon.classList.remove('bx-x');
 navbar.classList.remove('active');
 
 };
 
-
-/*========== swiper ==========*/
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 50,
     loop: true,
     grabCursor: true,
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    el: ".swiper-pagination",
+    clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
     },
 });
 
-
-/*========== dark light mode ==========*/
 let darkModeIcon = document.querySelector('#darkMode-icon');
 
 darkModeIcon.onclick = () => {
@@ -66,8 +55,6 @@ darkModeIcon.onclick = () => {
     document.body.classList.toggle('dark-mode');
 };
 
-
-/*========== scroll reveal ==========*/
 ScrollReveal({
     // reset: true,
     distance: '80px',
@@ -76,6 +63,6 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img img, .anggota-box, .vid-wrapper, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-info, .anggota-box, .vid-wrapper, .contact form, .card', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1', { origin: 'left' });
 ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
