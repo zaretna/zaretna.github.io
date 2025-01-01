c323d29959ffa1c31445384f8fd81ed3cbeb9269
zaretna <angelazaretna@gmail.com>
2025-01-01 14:13:24 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -56,13 +56,12 @@ darkModeIcon.onclick = () => {
 };
 
 ScrollReveal({
-    // reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-info, .anggota-box, .vid-wrapper, .contact form, .card', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img img, .anggota-box, .vid-wrapper, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1', { origin: 'left' });
 ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
